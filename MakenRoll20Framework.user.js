7a03eaea210d72ec76c53acbb63bf9e2a3670293
Lucas Magalhães <101735519+Make-it-fly@users.noreply.github.com>
2022-10-29 12:53:17 -0300

    Update MakenRoll20Framework.user.js

--- MakenRoll20Framework.user.js
+++ MakenRoll20Framework.user.js
@@ -15,6 +15,6 @@
 //style.href = 'https://gitcdn.link/repo/Make-it-fly/e351f5be422c69d7f10ee4f20e428154/raw/bda61fca95835f16e2ce3af8e318c561a9474b16/MakenRoll20FrameworkStyle.css';
 //document.querySelector('head').appendChild(link);
 
-console.log('eu hein')
+console.log('?????????????')
 
 function okkkk(){}
