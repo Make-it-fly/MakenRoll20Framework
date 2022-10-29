baa8377b4404057d3af276ab656f8a23f6902e8a
Lucas <lucoder618@gmail.com>
2022-10-29 13:29:34 -0300

    teste

--- MakenRoll20Framework.user.js
+++ MakenRoll20Framework.user.js
@@ -15,11 +15,13 @@
 
 // APPLYING STYLE
 
-const mkfStyle = document.createElement('link');
-mkfStyle.rel = 'stylesheet';
-mkfStyle.type = 'text/css';
-mkfStyle.href = 'https://gitcdn.link/repo/Make-it-fly/e351f5be422c69d7f10ee4f20e428154/raw/bda61fca95835f16e2ce3af8e318c561a9474b16/MakenRoll20FrameworkStyle.css';
-document.querySelector('head').appendChild(mkfStyle);
+function applyingStyle(){
+    const mkfStyle = document.createElement('link');
+    mkfStyle.rel = 'stylesheet';
+    mkfStyle.type = 'text/css';
+    mkfStyle.href = 'https://gitcdn.link/repo/Make-it-fly/e351f5be422c69d7f10ee4f20e428154/raw/bda61fca95835f16e2ce3af8e318c561a9474b16/MakenRoll20FrameworkStyle.css';
+    document.querySelector('head').appendChild(mkfStyle);
+}
 
 // ATT BUTTON
 
@@ -33,4 +35,5 @@ function creatingAttButton(){
 }
 setTimeout(()=>{
     creatingAttButton()
+    applyingStyle()
 }, 5000)
