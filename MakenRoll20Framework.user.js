f2f58d08a59c21d05a89ace9f5fe4167b34d9db0
Lucas <lucoder618@gmail.com>
2022-10-29 19:33:27 -0300

    sofrimento

--- MakenRoll20Framework.user.js
+++ MakenRoll20Framework.user.js
@@ -10,30 +10,144 @@
 // ==/UserScript==
 
 
+// GLOBAL TEMPLATES
+const mkfComponentsRegister = [
+    {
+        componentName: "test",
+        functionToExecute: (noteEditor)=>{
+            noteEditor.innerHTML += `<h2>eeeee</h2>`
+        } 
+    }
+];
 
+// GLOBAL STYLE
+
+const mkfStyle = `
+    
+`
+
+// INICIALIZAR
+
+setTimeout(()=>{
+    applyingStyle()
+}, 5000)
+
+
+// HANDLING JOURNAL ITENS
+
+window.addEventListener('click', (e)=>{
+    //Opening Handouts
+    if (e.target.closest("[data-itemid]") && e.target.closest("[data-itemid]").classList.contains('handout')) {
+        const itemId = e.target.closest("[data-itemid]").getAttribute("data-itemid");
+        Sheet.handleHandoutData(itemId);
+    }
+
+    // Opening Character Sheets
+    if (e.target.closest("[data-itemid]") && e.target.closest("[data-itemid]").classList.contains('character')) {
+        const itemId = e.target.closest("[data-itemid]").getAttribute("data-itemid");
+        console.log(itemId)
+        Sheet.handleCharacterSheetData(itemId);
+    }
+    // Saving changes on Character Sheets
+    /* if (e.target.closest(".ui-dialog") && e.target.closest(".ui-dialog").querySelector(".charactereditor")) {
+        const itemId = e.target.closest(".ui-dialog").querySelector("[data-characterid]").getAttribute("data-itemid");
+        console.log(itemId)
+        Sheet.handleCharacterSheetData(itemId);
+    } */
+})
+
+class Sheet {
+    static handleCharacterSheetData(itemId){
+        setTimeout(()=>{
+            const characterSheet = document.querySelector(`[data-characterid=${itemId}]`)
+            console.log(characterSheet)
+            const iframe = characterSheet.querySelector(`iframe`);
+            console.log(iframe)
+            const noteEditor = iframe.contentWindow.document.querySelector(".note-editor")
+            const notTreatedRawDataArr = noteEditor.querySelectorAll("pre");
+            const treatedRawDataArr = this.treatRawData([...notTreatedRawDataArr]);
+            const componentsArr = this.convertDataToComponent(treatedRawDataArr);
+        
+            this.cleanNoteEditorFromCode(noteEditor);
+        
+            componentsArr.forEach(component => {component.functionToExecute(noteEditor)})
+        },2000)
+        /* const loopingTryier = (itself) => {
+        }
+        loopingTryier(loopingTryier) */
+    }
+    static handleHandoutData(itemId){
+        const loopingTryier = (itself) => {
+            setTimeout(()=>{
+                const handout = document.querySelector(`[data-handoutid=${itemId}]`)
+                try {
+                    const noteEditor = handout.querySelector(".note-editor")
+                    const notTreatedRawDataArr = noteEditor.querySelectorAll("pre");
+                    const treatedRawDataArr = this.treatRawData([...notTreatedRawDataArr]);
+                    const componentsArr = this.convertDataToComponent(treatedRawDataArr);
+                
+                    this.cleanNoteEditorFromCode(noteEditor);
+                
+                    componentsArr.forEach(component => {component.functionToExecute(noteEditor)})
+                } catch (error) {
+                    itself(itself)
+                }
+            },1000)
+        }
+        loopingTryier(loopingTryier)
+    }
+
+    static treatRawData(notTreatedRawDataArr){
+        const treatedRawData = notTreatedRawDataArr.map((untreatedCodeBlock, i)=>{
+            return untreatedCodeBlock.innerText.replace(/\n/g, "")
+        })
+        return treatedRawData;
+    }
+    static convertDataToComponent(treatedRawDataArr){
+        const data = treatedRawDataArr.map((rawData)=>{
+            if (rawData.includes("#")) {
+                const rawData_removedTag = rawData.replace("#","")
+                const correctComponent = mkfComponentsRegister.find((component) => component.componentName == rawData_removedTag)
+                return correctComponent
+            }
+            if (rawData.includes("@")) {
+                
+            }
+        })
+        return data
+    }
+    static cleanNoteEditorFromCode(noteEditor){
+        const toRemove = noteEditor.querySelectorAll("pre");
+        [...toRemove].forEach(pre => pre.remove())
+    }
+}
 
 
 // APPLYING STYLE
 
 function applyingStyle(){
-    const mkfStyle = document.createElement('link');
-    mkfStyle.rel = 'stylesheet';
-    mkfStyle.type = 'text/css';
-    mkfStyle.href = 'https://gitcdn.link/repo/Make-it-fly/e351f5be422c69d7f10ee4f20e428154/raw/bda61fca95835f16e2ce3af8e318c561a9474b16/MakenRoll20FrameworkStyle.css';
-    document.querySelector('head').appendChild(mkfStyle);
+    const mkfStyleElement = document.createElement('style');
+    mkfStyleElement.innerText = mkfStyle;
+    document.head.appendChild(mkfStyleElement)
 }
+
+/* function applyingStyle(){
+    const mkfStyleElement = document.createElement('link');
+    mkfStyleElement.rel = 'stylesheet';
+    mkfStyleElement.type = 'text/css';
+    mkfStyleElement.href = 'https://gitcdn.link/cdn/Make-it-fly/MakenRoll20Framework/main/style.css';
+    document.querySelector('head').appendChild(mkfStyleElement);
+} */
 
 // ATT BUTTON
 
-function creatingAttButton(){
+/* function creatingAttButton(){
     const attButton = document.createElement('button');
     attButton.classList.add('btn-maken-teste');
     attButton.innerText = "Atualizar";
 
     document.querySelector('#playerzone').appendChild(attButton);
     console.log(attButton)
-}
-setTimeout(()=>{
-    creatingAttButton()
-    applyingStyle()
-}, 5000)
+} */
+
+
