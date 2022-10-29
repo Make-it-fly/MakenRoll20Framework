00d6c44097aab3aec2d756600cc3759dfc615367
Lucas <lucoder618@gmail.com>
2022-10-29 13:03:47 -0300

    teste

--- MakenRoll20Framework.user.js
+++ MakenRoll20Framework.user.js
@@ -20,3 +20,12 @@ style.rel = 'stylesheet';
 style.type = 'text/css';
 style.href = 'https://gitcdn.link/repo/Make-it-fly/e351f5be422c69d7f10ee4f20e428154/raw/bda61fca95835f16e2ce3af8e318c561a9474b16/MakenRoll20FrameworkStyle.css';
 document.querySelector('head').appendChild(link);
+
+// ATT BUTTON
+
+function creatingAttButton(){
+    const attButton = document.createElement('button');
+    attButton.classList.add('btn-maken-teste')
+    
+    document.querySelector('body').appendChild(attButton);
+}
