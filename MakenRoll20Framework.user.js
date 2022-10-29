ba7aa05a62871110b495deee75ea0fd04a496e99
Lucas Magalhães <101735519+Make-it-fly@users.noreply.github.com>
2022-10-29 12:37:55 -0300

    Update MakenRoll20Framework.user.js

--- MakenRoll20Framework.user.js
+++ MakenRoll20Framework.user.js
@@ -4,13 +4,15 @@
 // @version      0.1
 // @description  Create JSON from strings in roll20 lists
 // @author       Maken
-// @match        https://app.roll20.net/editor/
+// @match        http://127.0.0.1:5500/
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=google.com.br
 // @grant        none
 // ==/UserScript==
+//roll20: https://app.roll20.net/editor/
+//const style = document.createElement('link');
+//style.rel = 'stylesheet';
+//style.type = 'text/css';
+//style.href = 'https://gitcdn.link/repo/Make-it-fly/e351f5be422c69d7f10ee4f20e428154/raw/bda61fca95835f16e2ce3af8e318c561a9474b16/MakenRoll20FrameworkStyle.css';
+//document.querySelector('head').appendChild(link);
 
-const style = document.createElement('link');
-style.rel = 'stylesheet';
-style.type = 'text/css';
-style.href = 'https://gitcdn.link/repo/Make-it-fly/e351f5be422c69d7f10ee4f20e428154/raw/bda61fca95835f16e2ce3af8e318c561a9474b16/MakenRoll20FrameworkStyle.css';
-document.querySelector('head').appendChild(link);
+console.log('funfa')
