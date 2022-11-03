cc62ef8d37677472db727a796c852a7aadb79531
Lucas <lucoder618@gmail.com>
2022-11-02 22:34:58 -0300

    feat/attributes and abilities tab support

--- MakenRoll20Framework.user.js
+++ MakenRoll20Framework.user.js
@@ -9,43 +9,49 @@
 // @grant        none
 // ==/UserScript==
 
-// COMPONENT TEMPLATES
-class Templates {
-    static test (noteEditor){
-        noteEditor.innerHTML += `<h2 class="testandoMaken">eeeee</h2>`
-    } 
-}
 
-// COMPONENT DB
-
+// GLOBAL TEMPLATES
 const mkfComponentsRegister = [
     {
         componentName: "test",
-        componentTemplate: Templates.test
+        functionToExecute: (noteEditor)=>{
+            noteEditor.innerHTML += `<h2 class="testandoMaken">eeeee</h2>`
+        } 
     }
 ];
 
 // GLOBAL STYLE
 
 const mkfStyle = `
-    body{
-        font-family: 'Courier New', Courier, monospace;
-    }
-    .testandoMaken{
-        color: red;
-    }
-    .avatar{
-        float: none;
-    }
-    #bio-avatar{
-        float: none;
-    }
-    .commonWindow{
-        padding: 15px;
-        margin: 5px 5px;
-        border: 3px solid black;
-        display: block
-    }
+
+.avatar{
+    float: none;
+}
+#bio-avatar{
+    float: none;
+}
+.commonWindow{
+    position: relative;
+    padding: 15px;
+    margin: 5px 5px;
+    border: 3px solid black;
+    display: block;
+}
+.commonWindow .description-container{
+    display: flex;
+    justify-content: space-between;
+}
+.commonWindow .description-container .btn-macro{
+    border: 1px solid black;
+    padding: 5px 10px;
+    border-radius: 3px;
+    transition: .2s;
+    cursor: pointer;
+}
+.commonWindow .description-container .btn-macro:hover{
+    background-color: gray;
+    color: black;
+}
 `
 
 // INICIALIZAR
@@ -57,17 +63,20 @@ setTimeout(()=>{
 
 // HANDLING JOURNAL ITENS
 window.addEventListener('click', (e)=>{
-    //Opening Handouts
-    if (e.target.closest(".handout")) {
-        const itemId = e.target.closest("[data-itemid]").getAttribute("data-itemid");
-        Sheet.handleHandoutData(itemId);
-    }
     // Opening Character Sheets
     if (e.target.closest(".character")) {
         const itemId = e.target.closest("[data-itemid]").getAttribute("data-itemid");
         Sheet.handleCharacterSheetData(itemId);
         iframeApplyingStyle(itemId)
+        return
     }
+    //Opening Handouts
+    /* if (e.target.closest(".handout")) {
+        const itemId = e.target.closest("[data-itemid]").getAttribute("data-itemid");
+        Sheet.handleHandoutData(itemId);
+        return
+    } */
+    
 })
 window.addEventListener('message', (e) => {
     const itemId = e.data.characterId;
@@ -78,39 +87,41 @@ window.addEventListener('message', (e) => {
 class Sheet {
     static handleCharacterSheetData(itemId){
         setTimeout(()=>{
-            const iframe = document.querySelector(`[name='iframe_${itemId}']`);
-            const noteEditor = iframe.contentWindow.document.querySelector(".note-editor")
-            const notTreatedRawDataArr = noteEditor.querySelectorAll("pre");
-            const treatedRawDataArr = this.treatRawData([...notTreatedRawDataArr]);
-            const componentsArr = this.convertDataToComponent(treatedRawDataArr);
-        
-            this.cleanNoteEditorFromCode(noteEditor);
-        
-            componentsArr.forEach(component => {component.componentTemplate(noteEditor)})
+            try {
+                const iframe = document.querySelector(`[name='iframe_${itemId}']`);
+                const noteEditor = iframe.contentWindow.document.querySelector(".note-editor");
+                iframe.contentWindow.document.querySelector('[data-tab="attributesabilities"]').click()
+                let attributesTab = iframe.contentWindow.document.querySelector(".attributes").querySelector(".ui-sortable");
+                iframe.contentWindow.document.querySelector('[data-tab="bioinfo"]').click()
+                const notTreatedRawDataArr = noteEditor.querySelectorAll("pre");
+                const treatedRawDataArr = this.treatRawData([...notTreatedRawDataArr]);
+                const componentsArr = this.convertDataToComponent({treatedRawDataArr, attributesTab});
+            
+                this.cleanNoteEditorFromCode(noteEditor);
+            
+                componentsArr.forEach(component => {component.functionToExecute(noteEditor)})
+            } catch (error) {
+                console.log(error)
+            }
         },1000)
         /* const loopingTryier = (itself) => {
         }
         loopingTryier(loopingTryier) */
     }
     static handleHandoutData(itemId){
-        const loopingTryier = (itself) => {
-            setTimeout(()=>{
-                const handout = document.querySelector(`[data-handoutid=${itemId}]`)
-                try {
-                    const noteEditor = handout.querySelector(".note-editor")
-                    const notTreatedRawDataArr = noteEditor.querySelectorAll("pre");
-                    const treatedRawDataArr = this.treatRawData([...notTreatedRawDataArr]);
-                    const componentsArr = this.convertDataToComponent(treatedRawDataArr);
-                
-                    this.cleanNoteEditorFromCode(noteEditor);
-                
-                    componentsArr.forEach(component => {component.componentTemplate(noteEditor)})
-                } catch (error) {
-                    itself(itself)
-                }
-            },1000)
-        }
-        loopingTryier(loopingTryier)
+        setTimeout(()=>{
+            const handout = document.querySelector(`[data-handoutid=${itemId}]`);
+            console.log(handout)
+            const noteEditor = handout.querySelector(".note-editor");
+            console.log(noteEditor)
+            const notTreatedRawDataArr = noteEditor.querySelectorAll("pre");
+            const treatedRawDataArr = this.treatRawData([...notTreatedRawDataArr]);
+            const componentsArr = this.convertDataToComponent(treatedRawDataArr);
+        
+            this.cleanNoteEditorFromCode(noteEditor);
+        
+            componentsArr.forEach(component => {component.functionToExecute(noteEditor)})
+        },1000)
     }
     static treatRawData(notTreatedRawDataArr){
         const treatedRawData = notTreatedRawDataArr.map((untreatedCodeBlock, i)=>{
@@ -118,12 +129,17 @@ class Sheet {
         })
         return treatedRawData;
     }
-    static convertDataToComponent(treatedRawDataArr){
+    static convertDataToComponent({treatedRawDataArr, attributesTab}){
         const data = treatedRawDataArr.map((rawData)=>{
             if (rawData.includes("#")) {
                 const rawData_removedTag = rawData.replace("#","")
                 const correctComponent = mkfComponentsRegister.find((component) => component.componentName == rawData_removedTag)
-                return correctComponent
+                return {
+                    componentName: correctComponent.componentName,
+                    functionToExecute: correctComponent.functionToExecute,
+                    attributesTab: attributesTab
+                }
+                /* return correctComponent */
             }
             if (rawData.includes('$')) {
                 const type = rawData.replace(/\n/,"").match(/{(.)*}/)[0];
@@ -131,32 +147,12 @@ class Sheet {
                 const noHeaderData = rawData.replace("$","").replace(/{(.)*}/,"")
                 const untreatedDataArr = noHeaderData.split("@")
                 console.log(jsonType)
-                if (jsonType.type == "default" || jsonType.type == "") {
-                    return {
-                        componentName: "default",
-                        componentTemplate: (noteEditor)=>{
-                            noteEditor.innerHTML += `
-                                <div class="commonWindow">
-                                    <h2>${jsonType.title}</h2>
-                                    ${untreatedDataArr.map(data=>{
-                                        if (data.includes(':')) {
-                                            const [firstPart, secondPart] = data.split(':');
-                                            return `<p><b>${firstPart}:</b> ${secondPart}</p>`
-                                        } else {
-                                            return `<p>${data}</p>`
-                                        }
-                                    }).join('')}
-                                </div>
-                            `
-                        }
-                    }
-                }
+                return complexComponents({
+                    jsonType: jsonType,
+                    untreatedDataArr: untreatedDataArr,
+                    attributesTab: attributesTab
+                })
             }
-            /* if (rawData.match(/\$(.)*;/)[0]) {
-                const type = rawData.match(/\$(.)*;/)[0];
-                const 
-                console.log(type)
-            } */
         })
         return data
     }
@@ -165,8 +161,6 @@ class Sheet {
         [...toRemove].forEach(pre => pre.remove())
     }
 }
-
-
 // APPLYING STYLE
 
 function applyingStyle(){
@@ -184,21 +178,29 @@ function iframeApplyingStyle(itemId){
     },2000) 
 }
 
-/* function applyingStyle(){
-    const mkfStyleElement = document.createElement('link');
-    mkfStyleElement.rel = 'stylesheet';
-    mkfStyleElement.type = 'text/css';
-    mkfStyleElement.href = 'https://gitcdn.link/cdn/Make-it-fly/MakenRoll20Framework/main/style.css';
-    document.querySelector('head').appendChild(mkfStyleElement);
-} */
-
-// ATT BUTTON
-
-/* function creatingAttButton(){
-    const attButton = document.createElement('button');
-    attButton.classList.add('btn-maken-teste');
-    attButton.innerText = "Atualizar";
-
-    document.querySelector('#playerzone').appendChild(attButton);
-    console.log(attButton)
-} */
+function complexComponents({jsonType, untreatedDataArr, attributesTab}){
+    if (jsonType.type == "default" || jsonType.type == "" || jsonType.type == undefined) {
+        console.log(attributesTab)
+        return {
+            componentName: "default",
+            functionToExecute: (noteEditor)=>{
+                noteEditor.innerHTML += `
+                    <div class="commonWindow">
+                        <div class="description-container">
+                            <h2>${jsonType.title}</h2>
+                            <button class="btn-macro" untreatedDataArr="${untreatedDataArr}">Imprimir</button>
+                        </div>
+                        ${untreatedDataArr.map(data=>{
+                            if (data.includes(':')) {
+                                const [firstPart, secondPart] = data.split(':');
+                                return `<p><b>${firstPart}:</b> ${secondPart}</p>`
+                            } else {
+                                return `<p>${data}</p>`
+                            }
+                        }).join('')}
+                    </div>
+                `
+            }
+        }
+    }
+}
